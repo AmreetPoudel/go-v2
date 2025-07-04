bf849e425ba18f25e6a6502fb9d6a2dc8257ca86
amrit poudel <aamreet12345@gmail.com>
2025-07-04 22:09:35 +0545

    read from file

---
+++ readingfromafile.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	filename := "sample.txt"
+	file, err := os.Open(filename)
+	defer func() {
+		fmt.Println("closing the file")
+		file.Close()
+	}()
+	if err != nil {
+		fmt.Println("error opening file: ", err)
+	}
+	buff := make([]byte, 1024)
+	_, err = file.Read(buff)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(buff))
+}
